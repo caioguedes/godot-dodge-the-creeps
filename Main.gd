e5c6e26ac38d8a65ebd00dff44fd29b2192e99ae
Caio Guedes <caio.cesar.g.souza@gmail.com>
2021-06-13 18:28:25 -0300

    Add Sound Effects and Color Background

--- Main.gd
+++ Main.gd
@@ -13,6 +13,8 @@ func game_over():
     $MobTimer.stop()
     $HUD.show_game_over()
     get_tree().call_group("mobs", "queue_free")
+    $Music.stop()
+    $DeathSound.play()    
 
 
 func new_game():
@@ -21,6 +23,7 @@ func new_game():
     $StartTimer.start();
     $HUD.update_score(score)
     $HUD.show_message("Get Ready")
+    $Music.play()
 
 
 func _on_ScoreTimer_timeout():
